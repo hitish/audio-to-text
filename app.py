f5f9e26c292502884e9bcce65c666f79e55bc3b9
hitish <hitish.singla@gmail.com>
2024-04-26 15:49:09 +0530

    using whisper to load

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import whisper
-import numpy as np
+import tempfile
 
 # Set up Whisper model
 model = whisper.load_model("base")  # Choose from "base", "medium", or "large"
@@ -10,13 +10,13 @@ model = whisper.load_model("base")  # Choose from "base", "medium", or "large"
 st.title("Speech to Text with Whisper")
 st.write("Upload audio File to 'Transcribe'")
 
-audio_file = st.file_uploader("Upload Audio File", type=["wav", "mp3","weba"])
-print(audio_file)
+audio_file = st.file_uploader("Upload Audio File", type=["wav", "mp3"])
+
 if audio_file:
-  audio = whisper.load_audio(audio_file)
-    
+  with tempfile.NamedTemporaryFile(delete=False, suffix="."+audio_file.name.split(".")[-1]) as temp_file:
+      temp_file.write(audio_file.read())
+  audio = whisper.load_audio(temp_file.name)
   transcript = model.transcribe(audio)
-  print(transcript)
   st.write("Transcription:", transcript["text"])
 
 
